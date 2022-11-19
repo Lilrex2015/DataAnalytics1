420cd92d7739d32a021bd9e77952dd56db30168c
Arif Mitha <lilrex2015@gmail.com>
2022-11-19 13:07:13 -0700

    added intro lines and imports

---
+++ DA1_HW4/main.R
@@ -0,0 +1,24 @@
+#Data Analytics - 636 - HW4
+#Author -  Arif Mitha
+# Date 11/18/2022
+
+
+
+install.packages("ggplot2" , "dplyr")
+
+
+library(ggplot2)
+library(dplyr)
+
+
+load('salesData.rda')
+
+
+
+##############################################
+############### QUESTION 1 ###################
+##############################################
+##############################################
+
+
+#Q1A
